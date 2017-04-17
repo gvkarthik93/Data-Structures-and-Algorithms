c39c0dfd1a8358669474621b52b649b8a90bc452
Venkata Karthik Gullapalli <gvkarthik93@ymail.com>
2017-04-18 01:45:01 +0530

    Adding selection sort

--- Sorting.cpp
+++ Sorting.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 void bubbleSort();
 void modifiedBubbleSort();
+void selectionSort();
 
 int main() {
     bubbleSort();
     cout<<endl;
     modifiedBubbleSort();
+    cout<<endl;
+    selectionSort();
     cout<<endl;
     return 0;
 }
@@ -53,6 +56,31 @@ void modifiedBubbleSort() {
         }
         if (flag == 0)
             break;
+    }
+
+    for (int k=0;k<n;k++)
+    {
+        cout<<arr[k]<<" ";
+    }
+}
+
+void selectionSort() {
+    int arr[5] = {2,5,1,4,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i=0; i<n; i++)
+    {
+        int min_index = i;
+        for (int j=i; j<n;j++)
+        {
+            if (arr[min_index] > arr[j])
+            {
+                min_index = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
     }
 
     for (int k=0;k<n;k++)
