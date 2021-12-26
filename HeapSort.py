3b9bcc01f1ece190106ff700b1aad7a707be6937
Karthik <gvkarthik93@gmail.com>
2021-12-25 19:07:42 -0500

    Updating Heap Sort cod4e

--- HeapSort.py
+++ HeapSort.py
@@ -28,7 +28,7 @@ def heapSort(arr):
     n = len(arr)
  
     # Build a maxheap.
-    for i in range(n, -1, -1):
+    for i in range(n//2, -1, -1):
         heapify(arr, n, i)
  
     # One by one extract elements
@@ -37,8 +37,8 @@ def heapSort(arr):
         heapify(arr, i, 0)
  
 # Driver code to test above
-arr = [ 12, 11, 13, 5, 6, 7]
+arr = [ 12, 11, 13, 5, 6, 7, 12, -3, -5, -1, 0]
 heapSort(arr)
-n = len(arr)
+
 print ("Sorted array is")
 print (arr)
